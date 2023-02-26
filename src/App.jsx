251168317ecbcdeda6b45819001ce3a069d9a0b0
Peggy Kalliani <pegztales@gmail.com>
2023-02-26 19:43:04 +0200

    adding edit tasks feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 // custom components
 import CustomForm from './components/CustomForm'
+import EditForm from './components/EditForm'
 import TaskList from './components/TaskList'
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [previousFocusEl, setPreviousFocusEl] = useState(null);
+  const [editedTask, setEditedTask] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
 
   const addTask = (task) => {
     setTasks(prevState => [...prevState, task])
@@ -15,16 +19,55 @@ function App() {
     setTasks(prevState => prevState.filter(t => t.id !== id));
   }
 
+  const toggleTask = (id) => {
+    setTasks(prevState => prevState.map(t => (
+      t.id === id
+        ? { ...t, checked: !t.checked }
+        : t
+    )))
+  }
+
+  const updateTask = (task) => {
+    setTasks(prevState => prevState.map(t => (
+      t.id === task.id
+        ? { ...t, name: task.name }
+        : t
+    )))
+    closeEditMode();
+  }
+
+  const closeEditMode = () => {
+    setIsEditing(false);
+    previousFocusEl.focus();
+  }
+
+  const enterEditMode = (task) => {
+    setEditedTask(task);
+    setIsEditing(true);
+    setPreviousFocusEl(document.activeElement);
+  }
+
   return (
     <div className="container">
       <header>
         <h1>My Task List</h1>
       </header>
+      {
+        isEditing && (
+          <EditForm
+            editedTask={editedTask}
+            updateTask={updateTask}
+            closeEditMode={closeEditMode}
+          />
+        )
+      }
       <CustomForm addTask={addTask}/>
       {tasks && (
         <TaskList
           tasks={tasks}
           deleteTask={deleteTask}
+          toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
         />
       )}
     </div>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 // component import
 import TaskItem from './TaskItem';
 
 // styles
 import styles from './TaskList.module.css';
 
-const TaskList = ({ tasks, deleteTask }) => {
+const TaskList = ({ tasks, deleteTask, toggleTask, enterEditMode }) => {
   return (
     <ul className={styles.tasks}>
       {tasks.sort((a, b) => b.id - a.id).map(task => (
@@ -14,6 +12,8 @@ const TaskList = ({ tasks, deleteTask }) => {
           key={task.id}
           task={task}
           deleteTask={deleteTask}
+          toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
         />
       ))
       }
